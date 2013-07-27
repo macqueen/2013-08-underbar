8b537af707143f7aaf09e8dff03c27e5beae2e3d
macqueen <jessmacqueen@gmail.com>
2013-07-27 08:57:36 -0700

    updated filter function

--- src/underbar.js
+++ src/underbar.js
@@ -64,6 +64,13 @@ var _ = { };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, iterator) {
+    var newCollection = [];
+    _.each(collection, function(value, key, collection){
+      if (iterator(value)) {
+        newCollection.push(value);
+      }
+    });
+    return newCollection;
   };
 
   // Return all elements of an array that don't pass a truth test.
